2647400e5fd8fc7fbb158adf868c7d84c1a73752
Reavenk <Reavenk@gmail.com>
2021-01-11 13:58:07 -0600

    Original commit

---
+++ Assets/Scenes/Main.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // https://web.archive.org/web/20160116150939/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
+    public Texture2D tex;
+    public Texture2D tex2;
+    int idx = 0;
+    const int dim = 256;
+
+    public float divAmt = 50.0f;
+
+    public UnityEngine.UI.RawImage img;
+
+
+    void Start()
+    {
+        this.tex = new Texture2D(dim, dim);
+        this.tex2 = new Texture2D(dim, dim);
+
+        for(int i = 0; i < dim; ++i)
+        { 
+            for(int j = 0; j < dim; ++j)
+            { 
+                tex.SetPixel(i, j, new Color(0.0f, 0.0f, 0.0f, 0.0f));
+                tex2.SetPixel(i, j, new Color(0.0f, 0.0f, 0.0f, 0.0f));
+            }
+        }
+        tex.Apply();
+        tex2.Apply();
+    }
+
+    void Update()
+    {
+        this.Integrate();
+    }
+
+    void Integrate()
+    { 
+        int id2 = (idx + 1) % 2;
+
+
+        Texture2D cur = idx == 0 ? tex : tex2;
+        Texture2D oth = idx == 0 ? tex2 : tex;
+
+        float f = Mathf.Sin(Time.time * 2.0f * Mathf.PI) * 10.0f;
+        cur.SetPixel((int)(100 + (dim / divAmt) * 1), dim / 2, new Color(f, f, f, f));
+        cur.SetPixel((int)(100 + (dim / divAmt) * 2), dim / 2, new Color(f, f, f, f));
+        cur.SetPixel((int)(100 + (dim / divAmt) * 3), dim / 2, new Color(f, f, f, f));
+        cur.SetPixel((int)(100 + (dim / divAmt) * 4), dim / 2, new Color(f, f, f, f));
+        cur.SetPixel((int)(100 + (dim / divAmt) * 5), dim / 2, new Color(f, f, f, f));
+        cur.SetPixel((int)(100 + (dim / divAmt) * 6), dim / 2, new Color(f, f, f, f));
+        cur.SetPixel((int)(100 + (dim / divAmt) * 7), dim / 2, new Color(f, f, f, f));
+
+
+
+        //if(Input.GetKey(KeyCode.A) == true)
+        //{ 
+        //    float f = 1.0f;
+        //    cur.SetPixel(dim/2, dim/2, new Color(f, f, f, f));
+        //}
+
+        for (int y = 1; y < cur.height - 1; ++y )
+        { 
+            for(int x = 1; x < cur.width - 1; ++x)
+            {
+               float b2 = 
+                    ((
+                        cur.GetPixel(x - 1, y + 0).r + 
+                        cur.GetPixel(x + 1, y + 0).r + 
+                        cur.GetPixel(x + 0, y - 1).r + 
+                        cur.GetPixel(x + 0, y + 1).r) / 2.0f - oth.GetPixel(x, y).r) * 0.9999999701f;
+
+                oth.SetPixel(x, y, new Color(b2, b2, b2, b2));
+            }
+        }
+
+        oth.Apply();
+        img.texture = oth;
+
+        Texture2D swp = tex;
+        tex = tex2;
+        tex2 = swp;
+
+    }
+}
---
+++ Assets/SimShader.shader
@@ -0,0 +1,65 @@
+﻿Shader "Unlit/SimShader"
+{
+    Properties
+    {
+        _T1 ("Texture", 2D) = "white" {}
+        _T2("Texture", 2D) = "white" {}
+        _Inputs("Inputs", 2D) = "white" {}
+        _Bounds("bound", 2D) = "white" {}
+
+        _Dim("Sim Resolution", Vector) = (0,0,0,0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _T1;
+            sampler2D _T2;
+            float4 _T1_ST;
+            float4 _T2_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_T1, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
