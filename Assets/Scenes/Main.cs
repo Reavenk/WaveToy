8c4d209cb7c93a5a5bb1bee7cb3557db774e2865
Reavenk <Reavenk@gmail.com>
2021-01-11 20:10:10 -0600

    Implement texture & started UI

--- Assets/Scenes/Main.cs
+++ Assets/Scenes/Main.cs
@@ -5,84 +5,118 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     // https://web.archive.org/web/20160116150939/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
-    public Texture2D tex;
-    public Texture2D tex2;
+    //public Texture2D tex;
+    //public Texture2D tex2;
+
+    public RenderTexture [] renderTargs;
+    public RenderTexture inputTarg;
+    public RenderTexture simObstacles;
+
     int idx = 0;
-    const int dim = 256;
+    const int dim = 1024;
 
     public float divAmt = 50.0f;
 
     public UnityEngine.UI.RawImage img;
 
+    public Material simMat;
+    public Material drawMat;
+
+    public Camera simInputCam;
+    public Camera simObstablesCam;
+
+    public float modulateRate = 5.0f;
+
+    public PxPre.UIDock.Root dockSys;
+
+    private void Awake()
+    {
+        this.renderTargs = 
+            new RenderTexture[]
+            { 
+                new RenderTexture(dim, dim, 32, RenderTextureFormat.ARGBFloat),
+                new RenderTexture(dim, dim, 32, RenderTextureFormat.ARGBFloat),
+                new RenderTexture(dim, dim, 32, RenderTextureFormat.ARGBFloat)
+            };
+
+        foreach(Texture t in this.renderTargs)
+        {
+            t.wrapMode = TextureWrapMode.Clamp;
+        }
+
+        this.inputTarg = new RenderTexture(dim, dim, 32, RenderTextureFormat.ARGBFloat);
+        this.simInputCam.targetTexture = this.inputTarg;
+
+        this.simObstacles = new RenderTexture(dim, dim, 32, RenderTextureFormat.ARGBFloat);
+        this.simObstablesCam.targetTexture = this.simObstacles;
+
+    }
 
     void Start()
     {
-        this.tex = new Texture2D(dim, dim);
-        this.tex2 = new Texture2D(dim, dim);
+        RenderTexture.active = this.renderTargs[0];
+        GL.Clear(false, true, Color.black);
+        RenderTexture.active = this.renderTargs[1];
+        GL.Clear(false, true, Color.black);
+        RenderTexture.active = this.renderTargs[2];
+        GL.Clear(false, true, Color.black);
 
-        for(int i = 0; i < dim; ++i)
-        { 
-            for(int j = 0; j < dim; ++j)
-            { 
-                tex.SetPixel(i, j, new Color(0.0f, 0.0f, 0.0f, 0.0f));
-                tex2.SetPixel(i, j, new Color(0.0f, 0.0f, 0.0f, 0.0f));
-            }
-        }
-        tex.Apply();
-        tex2.Apply();
+        this.img.material.SetTexture("_Obs", this.simObstablesCam.targetTexture);
     }
 
     void Update()
     {
-        this.Integrate();
+        this.simInputCam.Render();
+        this.simObstablesCam.Render();
+
+        
+        simMat.SetTexture("_T1", this.renderTargs[0]);
+        simMat.SetTexture("_T2", this.renderTargs[1]);
+        simMat.SetTexture("_Input", this.inputTarg);
+        simMat.SetTexture("_Obs", this.simObstacles);
+        simMat.SetVector("_InvDim", new Vector4( 1.0f / (float)dim, 1.0f / (float)dim, 0.0f, 0.0f));
+
+        this.simMat.SetPass(0);
+        RenderTexture.active = this.renderTargs[2];
+        GL.PushMatrix();
+        GL.LoadIdentity();
+        GL.LoadOrtho();
+
+        GL.Begin(GL.QUADS);
+        GL.TexCoord2(0.0f, 0.0f);
+        GL.Vertex3(0.0f, 0.0f, 0.0f);
+
+        GL.TexCoord2(0.0f, 1.0f);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.TexCoord2(1.0f, 1.0f);
+        GL.Vertex3(1.0f, 1.0f, 0.0f);
+
+        GL.TexCoord2(1.0f, 0.0f);
+        GL.Vertex3(1.0f, 0.0f, 0.0f);
+        GL.End();
+
+        GL.PopMatrix();
+        RenderTexture.active = null;
+
+        this.renderTargs =
+            new RenderTexture[]
+            {
+                this.renderTargs[2],
+                this.renderTargs[0],
+                this.renderTargs[1]
+            };
+        
+        this.img.texture = this.renderTargs[0];
+
+        //simMat.SetTexture(
+        //this.Integrate();
+
+        float f = Mathf.Sin(Time.time * Mathf.PI * 2 * this.modulateRate) * 0.5f;
+        drawMat.SetColor("_Color", new Color(f, f, f, 1.0f));
     }
 
-    void Integrate()
-    { 
-        int id2 = (idx + 1) % 2;
-
-
-        Texture2D cur = idx == 0 ? tex : tex2;
-        Texture2D oth = idx == 0 ? tex2 : tex;
-
-        float f = Mathf.Sin(Time.time * 2.0f * Mathf.PI) * 10.0f;
-        cur.SetPixel((int)(100 + (dim / divAmt) * 1), dim / 2, new Color(f, f, f, f));
-        cur.SetPixel((int)(100 + (dim / divAmt) * 2), dim / 2, new Color(f, f, f, f));
-        cur.SetPixel((int)(100 + (dim / divAmt) * 3), dim / 2, new Color(f, f, f, f));
-        cur.SetPixel((int)(100 + (dim / divAmt) * 4), dim / 2, new Color(f, f, f, f));
-        cur.SetPixel((int)(100 + (dim / divAmt) * 5), dim / 2, new Color(f, f, f, f));
-        cur.SetPixel((int)(100 + (dim / divAmt) * 6), dim / 2, new Color(f, f, f, f));
-        cur.SetPixel((int)(100 + (dim / divAmt) * 7), dim / 2, new Color(f, f, f, f));
-
-
-
-        //if(Input.GetKey(KeyCode.A) == true)
-        //{ 
-        //    float f = 1.0f;
-        //    cur.SetPixel(dim/2, dim/2, new Color(f, f, f, f));
-        //}
-
-        for (int y = 1; y < cur.height - 1; ++y )
-        { 
-            for(int x = 1; x < cur.width - 1; ++x)
-            {
-               float b2 = 
-                    ((
-                        cur.GetPixel(x - 1, y + 0).r + 
-                        cur.GetPixel(x + 1, y + 0).r + 
-                        cur.GetPixel(x + 0, y - 1).r + 
-                        cur.GetPixel(x + 0, y + 1).r) / 2.0f - oth.GetPixel(x, y).r) * 0.9999999701f;
-
-                oth.SetPixel(x, y, new Color(b2, b2, b2, b2));
-            }
-        }
-
-        oth.Apply();
-        img.texture = oth;
-
-        Texture2D swp = tex;
-        tex = tex2;
-        tex2 = swp;
-
+    private void OnGUI()
+    {
     }
 }
--- Assets/SimShader.shader
+++ Assets/SimShader.shader
@@ -4,10 +4,10 @@
     {
         _T1 ("Texture", 2D) = "white" {}
         _T2("Texture", 2D) = "white" {}
-        _Inputs("Inputs", 2D) = "white" {}
-        _Bounds("bound", 2D) = "white" {}
+        _Input("Input", 2D) = "black" {}
+        _Obs("Obstacles", 2D) = "black"{}
 
-        _Dim("Sim Resolution", Vector) = (0,0,0,0)
+        _InvDim("Sim Incr", Vector) = (0,0,0,0)
     }
     SubShader
     {
@@ -33,31 +33,76 @@
             struct v2f
             {
                 float2 uv : TEXCOORD0;
-                UNITY_FOG_COORDS(1)
                 float4 vertex : SV_POSITION;
             };
 
             sampler2D _T1;
             sampler2D _T2;
+            sampler2D _Obs;
+            sampler2D _Input;
             float4 _T1_ST;
             float4 _T2_ST;
+            float2 _InvDim;
 
             v2f vert (appdata v)
             {
                 v2f o;
                 o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
-                UNITY_TRANSFER_FOG(o,o.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _T1);
                 return o;
             }
 
             fixed4 frag (v2f i) : SV_Target
             {
-                // sample the texture
-                fixed4 col = tex2D(_T1, i.uv);
-                // apply fog
-                UNITY_APPLY_FOG(i.fogCoord, col);
-                return col;
+                float4 pxTm1 = tex2D(_T2, i.uv);
+
+                float4 obj = tex2D(_Obs, i.uv);
+                if (obj.a != 0.0)
+                {
+                    float4 pxT = tex2D(_T2, i.uv);
+                    return pxT;
+                }
+
+                // KERNEL 1
+                //////////////////////////////////////////////////
+                float4 pxl = tex2D(_T1, i.uv + float2(-_InvDim.x, 0.0));
+
+                if (i.uv.x - _InvDim.x <= _InvDim.x * 0.5)
+                    pxl = pxTm1;
+
+                // KERNEL 2
+                //////////////////////////////////////////////////
+                float4 pxr = tex2D(_T1, i.uv + float2(_InvDim.x, 0.0));
+                if (i.uv.x + _InvDim.x >= 1.0 - _InvDim.x * 0.5)
+                    pxr = pxTm1;
+
+                // KERNEL 3
+                //////////////////////////////////////////////////
+                float4 pxt = tex2D(_T1, i.uv + float2(0.0, _InvDim.y));
+                if (i.uv.y + _InvDim.y >= 1.0 - _InvDim.y * 0.5)
+                    pxt = pxTm1;
+
+                // KERNEL 4
+                //////////////////////////////////////////////////
+                float4 pxb = tex2D(_T1, i.uv + float2(0.0, -_InvDim.y));
+                if (i.uv.y - _InvDim.y <= _InvDim.y * 0.5)
+                    pxb = pxTm1;
+
+                //////////////////////////////////////////////////
+                float4 pxC = (pxl + pxr + pxt + pxb);
+                float4 ip = tex2D(_Input, i.uv);
+                pxC = lerp(pxC, ip, ip.w);
+
+
+                //float4 v = ((pxl + pxr + pxt + pxb) / 2.0 - pxV) * 0.9999999701f;
+                float4 v = (pxC / 2.0 - pxTm1) * 0.9999f;
+                v = lerp(v, ip, ip.w);
+                //float4 v = float4(i.uv, 0, 1);// (pxl + pxr + pxt + pxb);
+
+                //v = abs(v);
+                //v = pxV;
+                //v.w = abs(v.w);
+                return v;
             }
             ENDCG
         }
